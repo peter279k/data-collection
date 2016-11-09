0004a338df28abc906fae28a737cf374792a07af
Ivo Petkov <ivo@ivopetkov.com>
2016-11-09 12:42:14 +0200

    Added support for dynamic properties.

--- src/DataObject.php
+++ src/DataObject.php
@@ -18,6 +18,7 @@ class DataObject implements \ArrayAccess
      * @var array 
      */
     private $data = [];
+    private $properties = [];
 
     /**
      * Constructs a new data object
@@ -29,52 +30,92 @@ class DataObject implements \ArrayAccess
         $this->data = $data;
     }
 
+    public function offsetGet($offset)
+    {
+        return $this->getProperty($offset);
+    }
+
     public function offsetSet($offset, $value)
     {
-        if (is_null($offset)) {
-            $this->data[] = $value;
-        } else {
-            $this->data[$offset] = $value;
+        if (!is_null($offset)) {
+            $this->setProperty($offset, $value);
         }
     }
 
     public function offsetExists($offset)
     {
-        return isset($this->data[$offset]);
+        return $this->isPropertySet($offset);
     }
 
     public function offsetUnset($offset)
     {
-        if (isset($this->data[$offset])) {
-            unset($this->data[$offset]);
-        }
-    }
-
-    public function offsetGet($offset)
-    {
-        return isset($this->data[$offset]) ? $this->data[$offset] : null;
-    }
-
-    public function __set($name, $value)
-    {
-        $this->data[$name] = $value;
+        $this->unsetProperty($offset);
     }
 
     public function __get($name)
     {
-        return isset($this->data[$name]) ? $this->data[$name] : null;
+        return $this->getProperty($name);
+    }
+
+    public function __set($name, $value)
+    {
+        $this->setProperty($name, $value);
     }
 
     public function __isset($name)
     {
-        return isset($this->data[$name]);
+        return $this->isPropertySet($name);
     }
 
     public function __unset($name)
     {
+        $this->unsetProperty($name);
+    }
+
+    private function getDynamicPropertyContext()
+    {
+        $context = new DataObject();
+        $context->object = $this;
+        $context->rawData = new DataObject($this->data);
+        return $context;
+    }
+
+    private function getProperty($name)
+    {
+        if (isset($this->properties[$name], $this->properties[$name][0])) {
+            $context = $this->getDynamicPropertyContext();
+            $context->propertyName = $name;
+            return call_user_func($this->properties[$name][0], $context);
+        }
+        return isset($this->data[$name]) ? $this->data[$name] : null;
+    }
+
+    private function setProperty($name, $value)
+    {
+        if (isset($this->properties[$name], $this->properties[$name][1])) {
+            $context = $this->getDynamicPropertyContext();
+            $context->propertyName = $name;
+            $this->data[$name] = call_user_func($this->properties[$name][1], $value, $context);
+            return;
+        }
+        $this->data[$name] = $value;
+    }
+
+    private function isPropertySet($name)
+    {
+        return isset($this->data[$name]);
+    }
+
+    private function unsetProperty($name)
+    {
         if (isset($this->data[$name])) {
             unset($this->data[$name]);
         }
+    }
+
+    public function registerProperty($name, $getCallback, $setCallback)
+    {
+        $this->properties[$name] = [$getCallback, $setCallback];
     }
 
 }
---
+++ tests/DataObjectTest.php
@@ -0,0 +1,150 @@
+<?php
+
+/*
+ * Data Collection
+ * https://github.com/ivopetkov/data-collection
+ * Copyright (c) 2016 Ivo Petkov
+ * Free to use under the MIT license.
+ */
+
+use IvoPetkov\DataCollection;
+use IvoPetkov\DataObject;
+
+/**
+ * @runTestsInSeparateProcesses
+ */
+class DataObjectTest extends DataCollectionTestCase
+{
+
+    /**
+     *
+     */
+    public function testConstructor()
+    {
+        $data = [
+            'property1' => 'a',
+            'property2' => 'b',
+            'property3' => 'c'
+        ];
+        $object = new DataObject($data);
+        $this->assertTrue($object->property1 === 'a');
+        $this->assertTrue($object->property2 === 'b');
+        $this->assertTrue($object->property3 === 'c');
+        $this->assertTrue($object->property4 === null);
+    }
+
+    /**
+     *
+     */
+    public function testProperties()
+    {
+        $object = new DataObject();
+        $this->assertTrue($object->property1 === null);
+        $this->assertTrue($object->property2 === null);
+        $this->assertTrue($object->property3 === null);
+        $this->assertFalse(isset($object->property1));
+        $this->assertFalse(isset($object->property2));
+        $this->assertFalse(isset($object->property3));
+        $object->property1 = 'a';
+        $object->property2 = 'b';
+        $this->assertTrue($object->property1 === 'a');
+        $this->assertTrue($object->property2 === 'b');
+        $this->assertTrue($object->property3 === null);
+        $this->assertTrue(isset($object->property1));
+        $this->assertTrue(isset($object->property2));
+        $this->assertFalse(isset($object->property3));
+        unset($object->property2);
+        $this->assertTrue($object->property1 === 'a');
+        $this->assertTrue($object->property2 === null);
+        $this->assertTrue($object->property3 === null);
+        $this->assertTrue(isset($object->property1));
+        $this->assertFalse(isset($object->property2));
+        $this->assertFalse(isset($object->property3));
+        $object->property1 = 'aa';
+        $object->property1 = 'aaa';
+        $object->property2 = 'b';
+        $this->assertTrue($object->property1 === 'aaa');
+        $this->assertTrue($object->property2 === 'b');
+        $this->assertTrue($object->property3 === null);
+        $this->assertTrue(isset($object->property1));
+        $this->assertTrue(isset($object->property2));
+        $this->assertFalse(isset($object->property3));
+    }
+
+    /**
+     *
+     */
+    public function testArrayAccess()
+    {
+        $object = new DataObject();
+        $this->assertTrue($object['property1'] === null);
+        $this->assertTrue($object['property2'] === null);
+        $this->assertTrue($object['property3'] === null);
+        $this->assertFalse(isset($object['property1']));
+        $this->assertFalse(isset($object['property2']));
+        $this->assertFalse(isset($object['property3']));
+        $object['property1'] = 'a';
+        $object['property2'] = 'b';
+        $this->assertTrue($object['property1'] === 'a');
+        $this->assertTrue($object['property2'] === 'b');
+        $this->assertTrue($object['property3'] === null);
+        $this->assertTrue(isset($object['property1']));
+        $this->assertTrue(isset($object['property2']));
+        $this->assertFalse(isset($object['property3']));
+        unset($object['property2']);
+        $this->assertTrue($object['property1'] === 'a');
+        $this->assertTrue($object['property2'] === null);
+        $this->assertTrue($object['property3'] === null);
+        $this->assertTrue(isset($object['property1']));
+        $this->assertFalse(isset($object['property2']));
+        $this->assertFalse(isset($object['property3']));
+        $object['property1'] = 'aa';
+        $object['property1'] = 'aaa';
+        $object['property2'] = 'b';
+        $this->assertTrue($object['property1'] === 'aaa');
+        $this->assertTrue($object['property2'] === 'b');
+        $this->assertTrue($object['property3'] === null);
+        $this->assertTrue(isset($object['property1']));
+        $this->assertTrue(isset($object['property2']));
+        $this->assertFalse(isset($object['property3']));
+    }
+
+    /**
+     *
+     */
+    public function testRegisterProperty()
+    {
+        $object = new DataObject();
+        $object->registerProperty('property1', function($context) {
+            if ($context->rawData->property1 === null) {
+                return 'unknown';
+            } else {
+                return $context->rawData->property1;
+            }
+        }, function($value) {
+            return $value + 1;
+        });
+        $this->assertTrue($object->property1 === 'unknown');
+        $object->property1 = 10;
+        $this->assertTrue($object->property1 === 11);
+    }
+
+    /**
+     *
+     */
+    public function testDynamicPropertyContext()
+    {
+        $object = new DataObject();
+        $object->registerProperty('property1', function($context) {
+            return $context->rawData->property2;
+        }, function($value, $context) {
+            $context->object->property2 = $value;
+        });
+        $object->property2 = 5;
+        $this->assertTrue($object->property1 === 5);
+        $object->property1 = 6;
+        $this->assertTrue($object->property1 === 6);
+        $this->assertTrue($object->property2 === 6);
+    }
+
+}
